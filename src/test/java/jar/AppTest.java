33bc2cb170cd3fdc5a5d54c38f04aa0d8341ddbd
mariapizarro1 <maria.pizarro.gomez@alumnos.upm.es>
2022-11-07 19:49:44 +0100

    update junit test

--- src/test/java/jar/AppTest.java
+++ src/test/java/jar/AppTest.java
@@ -1,8 +1,17 @@
 package jar;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Vector;
 
 import org.junit.Test;
+import org.mockito.Mockito;
+
+
 
 /**
  * Unit test for simple App.
@@ -17,4 +26,53 @@ public class AppTest
     {
         assertTrue( true );
     }
+    
+    @Test
+    public void excepcionVectorNulo() {
+    	Vector<Asignatura> vectorAsignaturas = new Vector<Asignatura>();
+    	Matricula matricula = new Matricula(vectorAsignaturas);
+    	assertThrows(Exception.class, () -> matricula.getImporte());
+    }
+    
+   
+    @Test
+    public void calculoImporteMatricula() {
+    
+    	Vector<Asignatura> vectorAsignaturas = new Vector<Asignatura>(); 
+    	Matricula matricula = new Matricula(vectorAsignaturas);
+    	
+    	Asignatura asignatura1 = Mockito.mock(Asignatura.class);
+    	when(asignatura1.getImporte()).thenReturn(1.0);
+        
+    	Asignatura asignatura2 = Mockito.mock(Asignatura.class);
+    	when(asignatura2.getImporte()).thenReturn(4.6);
+    	
+     	
+    	vectorAsignaturas.add(asignatura1);
+    	vectorAsignaturas.add(asignatura2);
+        assertEquals(matricula,5.6);
+        
+    }
+    
+    @Test
+    public void getImporteMatriculaVerify() throws Exception {
+    	Vector<Asignatura> vectorAsignaturas = new Vector<Asignatura>(); 
+    	Matricula matricula = new Matricula(vectorAsignaturas);
+    	
+    	Asignatura asignatura1 = Mockito.mock(Asignatura.class);
+    	when(asignatura1.getImporte()).thenReturn(1.0);
+        
+    	Asignatura asignatura2 = Mockito.mock(Asignatura.class);
+    	when(asignatura2.getImporte()).thenReturn(4.6);
+    	
+    	vectorAsignaturas.add(asignatura1);
+    	vectorAsignaturas.add(asignatura2);
+    	
+    	matricula.getImporte();
+    	verify(asignatura1).getImporte();
+        verify(asignatura2).getImporte();
+    }
+    
+    
+   
 }
